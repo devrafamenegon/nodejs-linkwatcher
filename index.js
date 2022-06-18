44f63f4b6707876eadcc6ec5e2002324979cd120
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-06-17 22:45:19 -0300

    :sparkles: Using Async/Await to get file to read

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ function handleError(e) {
   throw new Error(chalk.red(e.code, 'There is no file available in the path'));
 }
 
+async function getFile(filepath) {
+  const encoding = 'utf-8';
+  try {
+    const text = await fs.promises.readFile(filepath, encoding)
+    console.log(chalk.green(text));
+  } catch(error) {
+    handleError(error);
+  }
+}
+
+//usando .then
+/*
+function getFile(filepath) {
+  const encoding = 'utf-8'
+  fs.promises.readFile(filepath, encoding)
+  .then((data) => console.log(chalk.green(data)))
+  .catch((error) => handleError(error))
+}
+*/
+
+//sintaxe do fs
+/*
 function getFile(filepath) {
   const encoding = 'utf-8'
   fs.readFile(filepath, encoding, (error, data) => {
@@ -12,5 +34,6 @@ function getFile(filepath) {
     console.log(chalk.green(data));
   })
 }
+*/
 
 getFile('./arquivos/texto1.md');

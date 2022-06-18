f1df54bfd24fca403215c431a8dbca8e0766fe0e
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-06-17 23:41:13 -0300

    :sparkles: Capturing links

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+function extractLinks(text) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResults = [];
+
+  let temp;
+  while((temp = regex.exec(text)) !== null) {
+    arrayResults.push({
+      [temp[1]]: temp[2],
+    });
+  }
+  
+  return arrayResults;
+}
+
 function handleError(e) {
   throw new Error(chalk.red(e.code, 'There is no file available in the path'));
 }
@@ -9,7 +23,7 @@ async function getFile(filepath) {
   const encoding = 'utf-8';
   try {
     const text = await fs.promises.readFile(filepath, encoding)
-    console.log(chalk.green(text));
+    console.log(extractLinks(text));
   } catch(error) {
     handleError(error);
   }

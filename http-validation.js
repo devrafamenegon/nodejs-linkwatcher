15c255a3e0b07b22fd8f48b8f7c32b78de56bd5f
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-06-20 23:27:15 -0300

    :hammer: fix: now only one file is read as I couldn't continue the logic using multiple

--- http-validation.js
+++ http-validation.js
@@ -1,28 +1,55 @@
 //mantendo o padrão de import commonJS
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import('node-fetch')
+  .then(({default: fetch}) => fetch(...args));
+
+const chalk = require('chalk');
+
+function handleError(e) {
+  throw new Error(e.message);
+}
 
 async function statusCheck(urlArray) {
-  //Promises async await
-  const arrayStatus =  await Promise.all(urlArray.map(async url => {
-    const res = await fetch(url);
-    return res.status;
-  }))
-  return arrayStatus;
+  try {
+    //Promises async await
+    const arrayStatus =  await Promise
+    .all(urlArray.map(async url => {
+      const res = await fetch(url);
+      return (`${res.status} - ${res.statusText}`);
+    }))
+    return arrayStatus;
+  } catch(error) {
+    handleError(error);
+  }
+  
 }
 
 function urlGenerator(linksArray) {
-  let urls = [];
-  linksArray.forEach((linksChild) =>{
-    linksChild.map(linkObject => urls.push(Object.values(linkObject).join()));
-  })
-
-  return urls;
+  return linksArray.map(linkObject => (Object.values(linkObject).join()));
 }
 
 async function urlValidate(linksArray) {
   const links = urlGenerator(linksArray);
   const statusLinks = await statusCheck(links);
-  return statusLinks;
+
+  //spread operator
+  const results = linksArray.map((object, index) => ({
+    ...object, status: statusLinks[index] 
+  }))
+
+  return results;
 }
 
 module.exports = urlValidate;
+
+
+/*
+
+//lógica para desestruturar os dados do array 
+//que possui um array referente a cada página que guarda os links
+
+linksArray.forEach((linksChild) =>{
+      linksChild.map(linkObject => urls
+        .push(Object.values(linkObject).join()));
+  })
+
+  */
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const fs = require('fs');
-const path = require('path');
+//const path = require('path');
 
 function extractLinks(text) {
   const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
@@ -21,12 +21,30 @@ function handleError(e) {
 }
 
 async function getFile(filepath) {
+  const encoding = 'utf-8';
+  
+  try {
+    const text = await fs.promises.readFile(filepath, encoding);
+    return extractLinks(text);
+  } catch(error) {
+    return handleError(error);
+  }
+}
+
+module.exports = getFile;
+
+
+/*
+
+//função para ler todos os arquivos mardown que estiverem dentro da pasta ./arquivos
+
+async function getFile(filepath) {
   const absolutePath = path.join(__dirname, '.', filepath);
   const encoding = 'utf-8';
   
   try {
     const files = await fs.promises.readdir(absolutePath, { encoding });
-    console.log('arquivos', files);
+    console.log('files', files);
 
     const result = await Promise.all(files.map(async (file) => {
       const fileLocation = `${absolutePath}/${file}`;
@@ -39,32 +57,6 @@ async function getFile(filepath) {
   } catch(error) {
     return handleError(error);
   }
-}
-
-module.exports = getFile;
-
-
-
-/*
-
-//ALTERNATIVAS AO ASYNC/AWAIT-------------------------------
-//usando .then
-
-function getFile(filepath) {
-  const encoding = 'utf-8'
-  fs.promises.readFile(filepath, encoding)
-  .then((data) => console.log(chalk.green(data)))
-  .catch((error) => handleError(error))
-}
-
-//sintaxe do fs
-
-function getFile(filepath) {
-  const encoding = 'utf-8'
-  fs.readFile(filepath, encoding, (error, data) => {
-    if(error) handleError(error);
-    console.log(chalk.green(data));
-  })
 }
 
 */

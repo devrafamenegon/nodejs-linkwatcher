76a09a065724cb268d94e30b1107dbf09e715e62
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-06-21 23:22:19 -0300

    :hammer: fix: path file in second test

--- test/index.test.js
+++ test/index.test.js
@@ -11,8 +11,7 @@ describe('getFile', () => {
     expect(typeof getFile).toBe('function');
   })
   it('need to return array with results', async () => {
-    const result = await getFile('F:\Pessoal\Estudo\Alura\formacao_nodejs\nodejs_criando_biblioteca\test\arquivos\texto1.md')
-    console.log(result)
+    const result = await getFile('F:/Pessoal/Estudo/Alura/formacao_nodejs/nodejs_criando_biblioteca/test/arquivos/texto1.md')
     expect(result).toEqual(arrayResult)
   })
 })

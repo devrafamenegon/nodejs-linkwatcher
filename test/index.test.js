c709c278e08cb4a14189e1d1b74e61167775038f
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-06-21 23:20:34 -0300

    :sparkles: feat: testing getFile function

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,19 @@
 const getFile = require('../index');
 
-test('need to be a function', () => {
-  expect(typeof getFile).toBe('function');
+const arrayResult = [
+  {
+    FileList : `https://developer.mozilla.org/pt-BR/docs/Web/API/FileList`
+  }
+]
+
+describe('getFile', () => {
+  it('need no be a function', () => {
+    expect(typeof getFile).toBe('function');
+  })
+  it('need to return array with results', async () => {
+    const result = await getFile('F:\Pessoal\Estudo\Alura\formacao_nodejs\nodejs_criando_biblioteca\test\arquivos\texto1.md')
+    console.log(result)
+    expect(result).toEqual(arrayResult)
+  })
 })
+
